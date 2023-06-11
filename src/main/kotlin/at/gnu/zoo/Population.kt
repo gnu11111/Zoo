67ce5b3ec52f3f553de82eab92a475468ca07590
gnu11111 <gnu@gnu.at>
2023-06-11 18:51:10 +0200

    different algorithm for crossover and mutation

--- src/main/kotlin/at/gnu/zoo/Population.kt
+++ src/main/kotlin/at/gnu/zoo/Population.kt
@@ -1,6 +1,5 @@
 package at.gnu.zoo
 
-import kotlin.math.min
 import kotlin.random.Random
 
 class Population(val blobs: List<Blob> = emptyList()) {
@@ -9,7 +8,7 @@ class Population(val blobs: List<Blob> = emptyList()) {
         val mutationRate = (100 * (world.context.blobs - world.context.survivors)) / world.context.blobs
         val population = mutableListOf<Blob>()
         val positions = mutableSetOf<Position>()
-        repeat(min(world.context.blobs, world.freeSpots())) {
+        repeat(world.context.blobs.coerceAtMost(world.freeSpots())) {
             var newBlob: Blob
             do {
                 newBlob = blobs.createOffspring(world, mutationRate)
@@ -27,33 +26,31 @@ class Population(val blobs: List<Blob> = emptyList()) {
         val position = Position.randomPosition(world)
         val blob = if (isEmpty()) Blob.randomBlob(world) else this[Random.nextInt(size)]
         val genom = blob.brain.genom
-        val select = Random.nextInt(1000)
-        if (select < 200)
-            return Blob(Brain(blob.brain.genom), position)
-        else if (select < 998 - mutationRate) {
-            val genom2 = if (isEmpty()) Blob.randomBlob(world).brain.genom else this[Random.nextInt(size)].brain.genom
-            if (genom.length == genom2.length) {
-                val cutPosition = Random.nextInt(genom.length)
-                val newBrain = Brain(genom.substring(0, cutPosition) + genom2.substring(cutPosition))
-                return Blob(newBrain, position)
-            }
-        }
+        val genom2 = if (isEmpty()) Blob.randomBlob(world).brain.genom else this[Random.nextInt(size)].brain.genom
+        val cutPosition = Random.nextInt(genom.length)
+        val newGenom = genom.substring(0, cutPosition) + genom2.substring(cutPosition)
+        if (Random.nextInt(10000) >= (world.context.genomSize * mutationRate))
+            return Blob(Brain(newGenom), position)
         world.context.mutations++
+        val mutatedGenom = mutateGenom(newGenom)
+        return Blob(Brain(mutatedGenom), position)
+    }
+
+    private fun mutateGenom(genom: String): String {
         val genomPosition = Random.nextInt(genom.length - 4) + 4
         val gene = Integer.toBinaryString(genom[genomPosition].toString().toInt(16))!!.padStart(4, '0')
         val genePosition = Random.nextInt(gene.length)
         val newValue = if (gene[genePosition] == '0') '1' else '0'
         val newGene = gene.substring(0, genePosition) + newValue + gene.substring(genePosition + 1)
-        val newGenom = genom.substring(0, genomPosition) + Integer.toHexString(Integer.parseInt(newGene, 2)) +
+        return genom.substring(0, genomPosition) + Integer.toHexString(Integer.parseInt(newGene, 2)) +
                 genom.substring(genomPosition + 1)
-        return Blob(Brain(newGenom), position)
     }
 
     companion object {
         fun randomPopulation(world: World, genom: String? = null): Population {
             val population = mutableListOf<Blob>()
             val positions = mutableSetOf<Position>()
-            repeat(min(world.context.blobs, world.freeSpots())) {
+            repeat(world.context.blobs.coerceAtMost(world.freeSpots())) {
                 var newBlob: Blob
                 do {
                     newBlob = Blob.randomBlob(world, genom)
